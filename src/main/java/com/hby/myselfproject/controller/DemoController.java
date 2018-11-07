2c7c5b44c0b84d6d34a36ade21ed84b73aa53d6c
huboyang <hby@learningleader.com.cn>
2018-11-07 11:40:57 +0800

    添加测试代码

--- src/main/java/com/hby/myselfproject/controller/DemoController.java
+++ src/main/java/com/hby/myselfproject/controller/DemoController.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.mapper.Condition;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.hby.myselfproject.entity.Shop;
 import com.hby.myselfproject.service.IShopService;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,8 +30,23 @@ public class DemoController {
 
     @RequestMapping("select")
     public String select(){
+        Shop s = new Shop();
         EntityWrapper ew = new EntityWrapper();
         List<Shop> list = shopService.selectList(ew);
         return list.toString();
+    }
+
+
+    @Test
+    public void dysjx(){
+        for(int i=1;i<5;i++) {
+            for (int k = 4; k >= i; k--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
 }
